f592074b53d4ea0769c0ab09463a06e0bea7ff7e
sinicaaaa <144157770+sinicaaaa@users.noreply.github.com>
2024-04-16 17:08:56 +0300

    2

--- Infrastructure/Repositories/BooksRepository.cs
+++ Infrastructure/Repositories/BooksRepository.cs
@@ -5,17 +5,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
     public class BooksRepository : IBooksRepository
     {
+        private ConsoleBookStoreContext _context;
+
+        public BooksRepository()
+        {
+            _context = new ConsoleBookStoreContext();
+        }
         public bool Add(Book book)
         {
-            using (ConsoleBookStoreContext db = new ConsoleBookStoreContext())
+            using (_context)
             {
-                db.Books.Add(book);
-                db.SaveChanges();
+                _context.Books.Add(book);
+                _context.SaveChanges();
                 return true;
             }
         }
@@ -32,14 +39,14 @@ namespace Infrastructure.Repositories
 
         public bool Remove(string tittle, out long book_ID)
         {
-            using (var context = new ConsoleBookStoreContext())
+            using (_context)
             {
-                var entityToDelete = context.Books.FirstOrDefault(b => b.Title.Trim().ToLower().Replace(" ", "") == tittle);
+                var entityToDelete = _context.Books.FirstOrDefault(b => b.Title.Trim().ToLower().Replace(" ", "") == tittle);
                 book_ID = entityToDelete != null ? entityToDelete.Book_ID : 0;
                 if (entityToDelete != null)
                 {
-                    context.Books.Remove(entityToDelete);
-                    context.SaveChanges();
+                    _context.Books.Remove(entityToDelete);
+                    _context.SaveChanges();
                     return true;
                 }
                 return false;
@@ -49,15 +56,15 @@ namespace Infrastructure.Repositories
 
         public bool Update(Book book)
         {
-            using (var context = new ConsoleBookStoreContext())
+            using (_context)
             {
-                var existingBook = context.Books.FirstOrDefault(b => b.Book_ID == book.Book_ID);
+                var existingBook = _context.Books.FirstOrDefault(b => b.Book_ID == book.Book_ID);
 
                 if (existingBook != null)
                 {
                     existingBook.Title = book.Title;
                     existingBook.Author = book.Author;
-                    context.SaveChanges();
+                    _context.SaveChanges();
                     return true; // Повертаємо true, якщо оновлення пройшло успішно
                 }
 
