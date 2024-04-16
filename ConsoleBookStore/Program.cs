29e91cefe301e054622babf7b71885ba51dd4e00
Kozhan Mykhailo <gatewayz87k@gmail.com>
2024-04-16 17:49:35 +0300

    # add a lot of features 2

--- ConsoleBookStore/Program.cs
+++ ConsoleBookStore/Program.cs
@@ -1,117 +1,15 @@
 ﻿using Infrastructure;
-using Infrastructure.Repositories;
-using DrawEngine;
+using Common.DrawEngine;
 
 
 while (1 > 0)
 {
-    Sale sale1 = new Sale();
-    Book book1 = new Book();
-    Draw engine = new Draw();
-    engine.Menu();
-    int choice = Convert.ToInt32(Console.ReadLine());
-    switch (choice)
-    {
-        case 1:
-            {
-                engine.Print();
-                engine.Sales();
-                choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1: 
-                        {
-                            Console.WriteLine("Введіть автора книги");
-                            book1.Author = Console.ReadLine();
-
-                            Console.WriteLine("Введіть назву книги");
-                            book1.Title = Console.ReadLine();
-
-                            Console.WriteLine("Введіть ціну книги");
-                            if (long.TryParse(Console.ReadLine(), out long result1))
-                                sale1.Price = result1;
-                            else
-                                Console.WriteLine();
-
-                            Console.WriteLine("Введіть кількість проданих примірників книги");
-                            if (long.TryParse(Console.ReadLine(), out long result2))
-                                sale1.Number_Of_Sales = result2;
-                            else
-                                Console.WriteLine();
-
-                            SalesRepository addSale = new SalesRepository();
-                            BooksRepository addBook = new BooksRepository();
-                            
-                            if (addSale.Add(sale1) == true && addBook.Add(book1) == true)
-                                Console.WriteLine("Запис успішно доданий");
-
-                            break;
-                        }
-                    case 2:
-                        {
-                            Console.WriteLine("Введіть назву книжки яку хочете видалити");
-                            string tittleRemove = Console.ReadLine().Trim().ToLower().Replace(" ", "");
-                            BooksRepository removeBook = new BooksRepository();
-                            SalesRepository removeSale = new SalesRepository();                            
-                           
-                            if (removeBook.Remove(tittleRemove, out long book_ID) == true && removeSale.Remove(book_ID) == true)                            
-                                Console.WriteLine("Запис успішно видалений");                           
-                            
-                            break;
-                        }
-                    case 3:
-                        {
-                            Console.WriteLine("Введіть назву книжки запис якої хочете редагувати");
-                            string tittleUpdate = Console.ReadLine().Trim().ToLower().Replace(" ", "");
-                            BooksRepository updateBook = new BooksRepository();
-                            SalesRepository updateSale = new SalesRepository();
-
-                            using (var context = new ConsoleBookStoreContext())
-                            {
-                                var entityToUpdate = context.Books.FirstOrDefault(b => b.Title.Trim().ToLower().Replace(" ", "") == tittleUpdate);
-                                long book_ID = entityToUpdate != null ? entityToUpdate.Book_ID : 0;
-                                Console.WriteLine("Введіть оновленого автора книги");
-                                entityToUpdate.Author = Console.ReadLine();
-
-                                Console.WriteLine("Введіть оновлену назву книги");
-                                entityToUpdate.Title = Console.ReadLine();
-
-                                var entityToUpdate2 = context.Sales.FirstOrDefault(s => s.Sale_ID == book_ID);
-
-                                Console.WriteLine("Введіть оновлену ціну книги");
-                                if (long.TryParse(Console.ReadLine(), out long result1))
-                                    entityToUpdate2.Price = result1;
-                                else
-                                    Console.WriteLine();
-
-                                Console.WriteLine("Введіть оновлену кількість проданих примірників книги");
-                                if (long.TryParse(Console.ReadLine(), out long result2))
-                                    entityToUpdate2.Number_Of_Sales = result2;
-                                else
-                                    Console.WriteLine();
-
-                                if (updateBook.Update(entityToUpdate) == true && updateSale.Update(entityToUpdate2) == true)
-                                    Console.WriteLine("Запис успішно змінений");                                
-                            } 
-                            break;
-                        }
-                    case 4:                        
-                        break;
-                    default: 
-                        {
-                            break;
-                        }
-                }
-                break;
-            }
-        case 2:
-            {
-                Console.WriteLine("Робота програми завершена");
-                return 0;
-            }
-        default:
-            break;
-    }
+    DrawEngine engine = new DrawEngine();
+    engine.PrintMenu();
+    int choice = engine.ReadEnteredValue();
+    engine.PrintSalesMenu();
+    engine.PrintMenu();
+    engine.PrintAllSales();
 }
 
 
--- DrawEngine/DrawEngine.cs
+++ DrawEngine/DrawEngine.cs
@@ -1,17 +1,40 @@
 ﻿using Infrastructure;
+using Infrastructure.Repositories;
 
-
-namespace DrawEngine
+namespace Common.DrawEngine
 {
-    public class Draw
-    {       
-        public void Menu()
+    public class DrawEngine
+    {
+        //private ConsoleBookStoreContext _context;
+        private ISalesRepository _salesRepository;
+        private IBooksRepository _booksRepository;
+
+        public DrawEngine()
+        {
+            _salesRepository = new SalesRepository();
+            _booksRepository = new BooksRepository();
+        }
+
+        public void PrintMenu()
         {
             Console.WriteLine("Оберіть пункт меню:");
             Console.WriteLine("1.Переглянути продажі книгарні");            
             Console.WriteLine("2.Вийти з програми");
         }
-        public void Sales()
+
+        public int ReadEnteredValue()
+        {
+            try
+            {
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception ex) { 
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        public void PrintSalesMenu()
         {
             Console.WriteLine("Оберіть пункт меню:");
             Console.WriteLine("1.Додати запис до таблиці");
@@ -19,22 +42,125 @@ namespace DrawEngine
             Console.WriteLine("3.Редактувати запис з таблиці");
             Console.WriteLine("4.Повернутися назад");
         }
-        public void Print()
-        {
-            using (ConsoleBookStoreContext db = new ConsoleBookStoreContext())
-            {
-                var booksAndSales = from book in db.Books
-                                    join sale in db.Sales on book.Book_ID equals sale.Sale_ID into bookSales
-                                    from sale in bookSales.DefaultIfEmpty()
-                                    select new { BookTitle = book.Title, BookAuthor = book.Author, SalePrice = sale != null ? sale.Price : 0, SaleNumberOfSales = sale != null ? sale.Number_Of_Sales : 0 };
 
-                foreach (var item in booksAndSales)
-                {
-                    Console.WriteLine($"{item.BookTitle}, {item.BookAuthor}, {item.SalePrice}, {item.SaleNumberOfSales}");
-                }
+        public void PrintAllSales()
+        {
+            var sales = _salesRepository.GetAll();
+            var books = _booksRepository.GetAll();
+
+            foreach ( var sale in sales )
+            {
+                var book = books.Single(b => sale.Book_ID == sale.Book_ID);
+                if (book == null)
+                    throw new Exception("book not found");
+
+                Console.WriteLine($"{book.Title}, {book.Author}, {sale.Price}, {sale.Number_Of_Sales}");
             }
         }
 
+        public void f1(int choice)
+        {
+            Sale sale1 = new Sale();
+            Book book1 = new Book();
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        PrintSalesMenu();
+                        PrintSalesMenu();
+                        choice = Convert.ToInt32(Console.ReadLine());
+                        switch (choice)
+                        {
+                            case 1:
+                                {
+                                    Console.WriteLine("Введіть автора книги");
+                                    book1.Author = Console.ReadLine();
+
+                                    Console.WriteLine("Введіть назву книги");
+                                    book1.Title = Console.ReadLine();
+
+                                    Console.WriteLine("Введіть ціну книги");
+                                    if (long.TryParse(Console.ReadLine(), out long result1))
+                                        sale1.Price = result1;
+                                    else
+                                        Console.WriteLine();
+
+                                    Console.WriteLine("Введіть кількість проданих примірників книги");
+                                    if (long.TryParse(Console.ReadLine(), out long result2))
+                                        sale1.Number_Of_Sales = result2;
+                                    else
+                                        Console.WriteLine();
+
+                                    SalesRepository addSale = new SalesRepository();
+                                    BooksRepository addBook = new BooksRepository();
+
+                                    if (addSale.Add(sale1) == true && addBook.Add(book1) == true)
+                                        Console.WriteLine("Запис успішно доданий");
+
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine("Введіть назву книжки яку хочете видалити");
+                                    string tittleRemove = Console.ReadLine().Trim().ToLower().Replace(" ", "");
+                                    BooksRepository removeBook = new BooksRepository();
+                                    SalesRepository removeSale = new SalesRepository();
+
+                                    if (removeBook.Remove(tittleRemove, out long book_ID) == true && removeSale.Remove(book_ID) == true)
+                                        Console.WriteLine("Запис успішно видалений");
+
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Console.WriteLine("Введіть назву книжки запис якої хочете редагувати");
+                                    string tittleUpdate = Console.ReadLine().Trim().ToLower().Replace(" ", "");
+                                    BooksRepository updateBook = new BooksRepository();
+                                    SalesRepository updateSale = new SalesRepository();
+
+                                    using (var context = new ConsoleBookStoreContext())
+                                    {
+                                        var entityToUpdate = context.Books.FirstOrDefault(b => b.Title.Trim().ToLower().Replace(" ", "") == tittleUpdate);
+                                        long book_ID = entityToUpdate != null ? entityToUpdate.Book_ID : 0;
+                                        Console.WriteLine("Введіть оновленого автора книги");
+                                        entityToUpdate.Author = Console.ReadLine();
+
+                                        Console.WriteLine("Введіть оновлену назву книги");
+                                        entityToUpdate.Title = Console.ReadLine();
+
+                                        var entityToUpdate2 = context.Sales.FirstOrDefault(s => s.Sale_ID == book_ID);
+
+                                        Console.WriteLine("Введіть оновлену ціну книги");
+                                        if (long.TryParse(Console.ReadLine(), out long result1))
+                                            entityToUpdate2.Price = result1;
+                                        else
+                                            Console.WriteLine();
+
+                                        Console.WriteLine("Введіть оновлену кількість проданих примірників книги");
+                                        if (long.TryParse(Console.ReadLine(), out long result2))
+                                            entityToUpdate2.Number_Of_Sales = result2;
+                                        else
+                                            Console.WriteLine();
+
+                                        if (updateBook.Update(entityToUpdate) == true && updateSale.Update(entityToUpdate2) == true)
+                                            Console.WriteLine("Запис успішно змінений");
+                                    }
+                                    break;
+                                }
+                            case 4:
+                                break;
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
     }
 
 }
